6810be9e3c9ba39dbf86a6feea6f15aa8cf54bd2
Laura Righi <laurab.righi@gmail.com>
2024-01-06 13:34:32 -0300

    feat: filters

--- articles/settings.py
+++ articles/settings.py
@@ -34,7 +34,6 @@ INSTALLED_APPS = [
     "rest_framework.authtoken",
     "rest_framework_simplejwt",
     "corsheaders",
-    "common",
     "user",
     "blog",
 ]
--- common/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-
-class BaseModel(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True)
-    modified_at = models.DateTimeField(auto_now=True, null=True, blank=True)
