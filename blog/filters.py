6eb0d3b562380e20581e438f658efcdbec83e0fe
Laura Righi <laurab.righi@gmail.com>
2024-01-07 16:03:31 -0300

    feat: script populate_db, images

--- blog/filters.py
+++ blog/filters.py
@@ -5,7 +5,7 @@ from blog.models import Articles
 
 class ArticleFilter(filters.FilterSet):
     title = filters.CharFilter(field_name='title', lookup_expr='icontains')
-    category = filters.CharFilter(field_name='article_category__id')
+    category = filters.NumberFilter(field_name='article_category__id')
 
     class Meta:
         model = Articles
